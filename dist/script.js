d2c42bc14c11d2626d4369fc24cd51f63d7be79c
Subham Sahu <about.subhamsahu@gmail.com>
2022-06-01 22:22:51 +0530

    01st June

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+"use strict";
+// const MJ: string = "Michelle Jones";
+// const herAge: number = 17;
+// const isSpiderMansGF: boolean = true;
+// const res: string = isSpiderMansGF ? "Spider Man's GF" : "ERROR!";
+// console.log(`\n${MJ} is ${res} & her age is ${herAge}\n`);
--- script.js
+++
@@ -1,5 +0,0 @@
-var MJ = "Michelle Jones";
-var herAge = 17;
-var isSpiderMansGF = true;
-var res = isSpiderMansGF ? "Spider Man's GF" : "ERROR!";
-console.log("\n".concat(MJ, " is ").concat(res, " & her age is ").concat(herAge, "\n"));
